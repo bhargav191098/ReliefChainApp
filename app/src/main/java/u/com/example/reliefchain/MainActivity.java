6c05a196bbefd5397826a76548dfcdb049be5651
Bhargav <bhargav3514@gmail.com>
2018-08-28 21:14:31 +0530

    .

--- app/src/main/java/u/com/example/reliefchain/MainActivity.java
+++ app/src/main/java/u/com/example/reliefchain/MainActivity.java
@@ -2,10 +2,17 @@ package u.com.example.reliefchain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public EditText aadhar_field;
+    public EditText mobile_field;
+    public Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        aadhar_field= (EditText) findViewById(R.id.input_aadhar);
+        mobile_field = (EditText) findViewById(R.id.input_phone);
+        submit = (Button)findViewById(R.id.btn_login);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String aadhar = aadhar_field.getText().toString();
+                String mobile = mobile_field.getText().toString();
+
+            }
+        });
+
+
 
     }
 }
